9dbae334a02d52cfc332f386a4d06b468fd65820
ienrum <azang0506@gmail.com>
2023-08-03 20:11:18 +0900

    feat: todoSlice 만듬
    createSlice 로 todo 만듦
    initalstate 로 Todo 배열을 가진 객체 넣어줌
    addTodo 와 removeTodo 리듀서 만듦

---
+++ store/todoSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface Todo {
+  id: string;
+  text: string;
+}
+
+interface TodoState {
+  todos: Todo[];
+}
+
+const initialState: TodoState = {
+  todos: [],
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      state.todos.push({ id: Date.now().toString(), text: action.payload });
+    },
+    removeTodo: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
