1dbe82f4945c0f6dfe98c8cabc0b284a3cc4b60a
ienrum <azang0506@gmail.com>
2023-08-03 20:02:38 +0900

    feat: 리덕스, 네비게이터 추가 app 작성

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import MainNavigator from "./routes/MainNavigator";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
+import { StyleSheet } from "react-native";
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Provider store={store}>
+        <MainNavigator />
+      </Provider>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
   },
 });
+
+export default App;
